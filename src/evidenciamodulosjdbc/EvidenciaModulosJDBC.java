881a2d960d1c3d6c704e4cfceaebcf0ee49b693e
lancero <casu042010.casu@gmail.com>
2023-07-27 11:52:07 -0500

    crud

--- src/evidenciamodulosjdbc/EvidenciaModulosJDBC.java
+++ src/evidenciamodulosjdbc/EvidenciaModulosJDBC.java
@@ -34,7 +34,7 @@ public class EvidenciaModulosJDBC {
         try {
             conexion = DriverManager.getConnection(url,usuario,password);
             statement = conexion.createStatement();
-            statement.executeUpdate("INSERT INTO USUARIOS(USUARIO, CONTRASEÑA) VALUES('MARIA',9999)");
+            statement.executeUpdate("INSERT INTO USUARIOS(USUARIO, CONTRASEÑA) VALUES('CARLOS',3945)");
             rs = statement.executeQuery("SELECT * FROM USUARIOS");
             rs.next();
             do{
@@ -48,4 +48,57 @@ public class EvidenciaModulosJDBC {
     
 }
 
-        // ... Código existente ...
+// ... Código existente ...
+
+try {
+    conexion = DriverManager.getConnection(url, usuario, password);
+    statement = conexion.createStatement();
+
+    // Eliminar usuario 'MARIA'
+    String usuarioAEliminar = "MARIA";
+    String sqlEliminarUsuario = "DELETE FROM USUARIOS WHERE USUARIO = '" + usuarioAEliminar + "'";
+    statement.executeUpdate(sqlEliminarUsuario);
+
+    // Mostrar los usuarios después de la eliminación
+    rs = statement.executeQuery("SELECT * FROM USUARIOS");
+    rs.next();
+    do {
+        System.out.println(rs.getInt("id") + " : " + rs.getString("usuario"));
+    } while (rs.next());
+
+} catch (SQLException ex) {
+    Logger.getLogger(EvidenciaModulosJDBC.class.getName()).log(Level.SEVERE, null, ex);
+}
+
+
+// ... Código existente ...
+
+try {
+    conexion = DriverManager.getConnection(url, usuario, password);
+    statement = conexion.createStatement();
+
+    // Actualizar el usuario 'MARIA' si existe
+    String usuarioAActualizar = "MARIA";
+    String nuevoNombre = "MARIA_MODIFICADO";
+    String sqlActualizarUsuario = "UPDATE USUARIOS SET USUARIO = ? WHERE USUARIO = ?";
+    PreparedStatement psActualizar = conexion.prepareStatement(sqlActualizarUsuario);
+    psActualizar.setString(1, nuevoNombre);
+    psActualizar.setString(2, usuarioAActualizar);
+    int filasAfectadas = psActualizar.executeUpdate();
+
+    // Comprobar si se actualizó el usuario 'MARIA'
+    if (filasAfectadas > 0) {
+        System.out.println("Usuario 'MARIA' actualizado correctamente.");
+    } else {
+        System.out.println("El usuario 'MARIA' no existe en la base de datos.");
+    }
+
+    // Mostrar los usuarios después de la actualización
+    rs = statement.executeQuery("SELECT * FROM USUARIOS");
+    while (rs.next()) {
+        System.out.println(rs.getInt("id") + " : " + rs.getString("usuario"));
+    }
+
+} catch (SQLException ex) {
+    Logger.getLogger(EvidenciaModulosJDBC.class.getName()).log(Level.SEVERE, null, ex);
+}
